d15e51592d1b5f235667c233308f8ac42c434ddf
raguljerish <46901224+raguljerish@users.noreply.github.com>
2019-02-07 11:56:59 +0530

    Rename Q2.py to Q2a.py

---
+++ Q2a.py
@@ -0,0 +1,11 @@
+#raguljerish
+n=int(input("enter a number less than 20:"))
+if n <=20:
+ i=1
+ multi=1
+ while i<=n:
+    multi=multi*i
+    i+=1
+ print(multi)
+else:
+  print ("enter values less than 20"
